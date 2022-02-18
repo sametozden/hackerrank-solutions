6f6354a87f197fae32e173319733ddd93e845466
Samet Özden <44625016+sametozden@users.noreply.github.com>
2022-02-18 19:15:22 +0300

    solutions

--- Algorithms/Implementation/number-line-jumps.php
+++ Algorithms/Implementation/number-line-jumps.php
@@ -1,19 +1,14 @@
 <?php
 function kangaroo($x1, $v1, $x2, $v2) {
-    $status = false;
     for($i=1; $i<=10000; $i++){
         $x1 += $v1;
         $x2 += $v2;
 
         if($x1 == $x2 ){
             return "YES";
-            $status = true;
-            break;
         }
     }
 
-    if($status == false){
-        return "NO";
-    }
+    return "NO";
 }
 ?>
