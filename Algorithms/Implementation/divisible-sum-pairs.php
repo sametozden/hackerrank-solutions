b9bb2a2cbf8324b3401fdd97349496c71062d6be
Samet Özden <44625016+sametozden@users.noreply.github.com>
2022-02-22 22:38:59 +0300

    change var name i2 to j

--- Algorithms/Implementation/divisible-sum-pairs.php
+++ Algorithms/Implementation/divisible-sum-pairs.php
@@ -3,8 +3,8 @@ function divisibleSumPairs($n, $k, $ar) {
     $pair = 0;
 
     for($i=0; $i<($n-1); $i++){
-        for($i2=$i+1; $i2<$n; $i2++){
-            if(($ar[$i] + $ar[$i2]) % $k == 0){
+        for($j=$i+1; $j<$n; $j++){
+            if(($ar[$i] + $ar[$j]) % $k == 0){
                 //echo $ar[$i]. "+". $ar[$i2]. "\n";
                 $pair++;
             }
